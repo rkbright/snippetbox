bfcbe1876d791bd6f577405d1ae05bb74cdce66c
richard bright <rkbright1@gmail.com>
2020-12-26 18:27:47 -0800

    ch 4.4

---
+++ pkg/models/mysql/snippets.go
@@ -0,0 +1,23 @@
+package mysql
+
+import (
+	"database/sql"
+
+	"github.com/rkbright/snippetbox/pkg/models"
+)
+
+type SnippetModel struct {
+	DB *sql.DB
+}
+
+func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
+	return 0, nil
+}
+
+func (n *SnippetModel) Get(id int) (*models.Snippet, error) {
+	return nil, nil
+}
+
+func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
+	return nil, nil
+}
