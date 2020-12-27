43f87a52364a087734411610543c8b9a254f12a7
richard bright <rkbright1@gmail.com>
2020-12-27 10:02:18 -0800

    ch 5.2

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -20,6 +20,26 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		app.serverError(w, err)
 		return
 	}
+
+	data := &templateData{Snippets: s}
+
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	err = ts.Execute(w, data)
+	if err != nil {
+		app.serverError(w, err)
+	}
+
 	for _, snippet := range s {
 		fmt.Fprintf(w, "%v\n", snippet)
 	}
